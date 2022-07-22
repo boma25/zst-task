23847a9c4f68ac4d036e9e2ae37ee23aa0e80bdd
boma <amakirisamuelboma@gmail.co>
2022-07-22 14:11:53 +0100

    added testing for toastify

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,22 @@ import "./index.css"
 import App from "./App"
 import { store } from "./store"
 import { Provider } from "react-redux"
+import { ToastContainer } from "react-toastify"
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
+			<ToastContainer
+				position="top-center"
+				autoClose={5000}
+				hideProgressBar
+				newestOnTop
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss={false}
+				draggable
+				pauseOnHover={false}
+			/>
 			<App />
 		</Provider>
 	</React.StrictMode>,
