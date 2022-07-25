de03575417f1ffbd50bdae3deffdea20360eafc8
boma <amakirisamuelboma@gmail.co>
2022-07-25 08:45:43 +0100

    updated"

--- src/_tests_/listing.test.js
+++ src/_tests_/listing.test.js
@@ -1,78 +1,69 @@
 /** @format */
 
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import App from "../App";
-import { useAppDispatch, useAppSelector } from "../store/hooks";
-import { testStateSelector } from "../store/test-utiles";
-import Listing from "../component/listing";
-import userEvent from "@testing-library/user-event";
+import { screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import App from "../App"
 
-jest.mock("../store/hooks");
+import Listing from "../component/listing"
+import userEvent from "@testing-library/user-event"
+import { renderWithProviders } from "../store/test-utiles"
 
 describe("listing", () => {
-  beforeEach(() => {
-    useAppSelector.mockImplementation(testStateSelector);
-    useAppDispatch.mockImplementation(() => jest.fn);
-  });
+	it("should render the app and the search component", () => {
+		renderWithProviders(<App />)
+		const inputs = screen.getAllByPlaceholderText("Search listings")
 
-  afterEach(() => jest.clearAllMocks());
+		inputs.forEach((input) => {
+			expect(input).toBeInTheDocument()
+		})
+	})
 
-  it("should render the app and the search component", () => {
-    render(<App />);
-    const inputs = screen.getAllByPlaceholderText("Search listings");
+	it("should render the app and the 'Listing' title", () => {
+		renderWithProviders(<App />)
 
-    inputs.forEach((input) => {
-      expect(input).toBeInTheDocument();
-    });
-  });
+		const texts = screen.getAllByText("Listings")
 
-  it("should render the app and the 'Listing' title", () => {
-    render(<App />);
+		texts.forEach((text) => {
+			expect(text).toBeInTheDocument()
+		})
+	})
 
-    const texts = screen.getAllByText("Listings");
+	it("should render the listing component and it should have an image, title, contact and location", () => {
+		renderWithProviders(
+			<Listing
+				name="5 bedroom Duplex in Ajah"
+				imageUrl="https://wp.zillowstatic.com/25/real-estate-photo-shutterstock_554000050-ca1d64.jpg"
+				description="Beautiful terrace located in a serene environment"
+				contact_name="Fred Benjamin"
+				contact_num={+2349031561232}
+			/>
+		)
 
-    texts.forEach((text) => {
-      expect(text).toBeInTheDocument();
-    });
-  });
+		const image = screen.getByAltText("listing-view")
 
-  it("should render the listing component and it should have an image, title, contact and location", () => {
-    render(
-      <Listing
-        name="5 bedroom Duplex in Ajah"
-        imageUrl="https://wp.zillowstatic.com/25/real-estate-photo-shutterstock_554000050-ca1d64.jpg"
-        description="Beautiful terrace located in a serene environment"
-        contact_name="Fred Benjamin"
-        contact_num={+2349031561232}
-      />
-    );
+		expect(image).toHaveAttribute(
+			"src",
+			"https://wp.zillowstatic.com/25/real-estate-photo-shutterstock_554000050-ca1d64.jpg"
+		)
+		expect(screen.getByText("5 bedroom Duplex in Ajah")).toBeInTheDocument()
+		expect(
+			screen.getByText("Beautiful terrace located in a serene environment")
+		).toBeInTheDocument()
+		expect(screen.getByText("+2349031561232")).toBeInTheDocument()
+	})
 
-    const image = screen.getByAltText("listing-view");
+	it("should open and close the contact modal", () => {
+		renderWithProviders(<App />)
 
-    expect(image).toHaveAttribute(
-      "src",
-      "https://wp.zillowstatic.com/25/real-estate-photo-shutterstock_554000050-ca1d64.jpg"
-    );
-    expect(screen.getByText("5 bedroom Duplex in Ajah")).toBeInTheDocument();
-    expect(
-      screen.getByText("Beautiful terrace located in a serene environment")
-    ).toBeInTheDocument();
-    expect(screen.getByText("+2349031561232")).toBeInTheDocument();
-  });
+		const listing = screen.getAllByTestId("click-listing")
 
-  it("should open and close the contact modal", () => {
-    render(<App />);
+		userEvent.click(listing[0])
+		expect(screen.getByTestId("contact-form")).toBeInTheDocument()
 
-    const listing = screen.getAllByTestId("click-listing");
+		const close = screen.queryByTestId("close-form")
+		expect(close).toBeInTheDocument()
 
-    userEvent.click(listing[0]);
-    expect(screen.getByTestId("contact-form")).toBeInTheDocument();
-
-    const close = screen.queryByTestId("close-form");
-    expect(close).toBeInTheDocument();
-
-    userEvent.click(close);
-    expect(screen.queryByTestId("contact-form")).not.toBeInTheDocument();
-  });
-});
+		userEvent.click(close)
+		expect(screen.queryByTestId("contact-form")).not.toBeInTheDocument()
+	})
+})
